f5ae6f2b21bfe4d8bd3ec43c8a75c03383e19a55
olawale-o <omoogunolawale@gmail.com>
2021-10-12 11:23:22 +0100

    Implement factorial method

--- solver.rb
+++ solver.rb
@@ -1,2 +1,11 @@
 class Solver
+  def factorial(number)
+    raise 'This method only accepts 0 and positive integers' if number.negative?
+
+    if number.zero?
+      1
+    else
+      number * factorial(number - 1)
+    end
+  end
 end
